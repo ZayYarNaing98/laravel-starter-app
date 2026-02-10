ccd2d95b035d3c767c600cf180296c4e9178e078
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-02-10 14:36:51 +0630

    Add Article Model and Migration

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,39 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = [
-            [
-                'id' => 1,
-                'title' => 'First Article',
-                'content' => 'This is the content of the first article.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Second Article',
-                'content' => 'This is the content of the second article.'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Third Article',
-                'content' => 'This is the content of the third article.'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Fourth Article',
-                'content' => 'This is the content of the fourth article.'
-            ],
-            [
-                'id' => 5,
-                'title' => 'Fifth Article',
-                'content' => 'This is the content of the fifth article.'
-            ]
-        ];
+        // $articles = [
+        //     [
+        //         'id' => 1,
+        //         'title' => 'First Article',
+        //         'content' => 'This is the content of the first article.'
+        //     ],
+        //     [
+        //         'id' => 2,
+        //         'title' => 'Second Article',
+        //         'content' => 'This is the content of the second article.'
+        //     ],
+        //     [
+        //         'id' => 3,
+        //         'title' => 'Third Article',
+        //         'content' => 'This is the content of the third article.'
+        //     ],
+        //     [
+        //         'id' => 4,
+        //         'title' => 'Fourth Article',
+        //         'content' => 'This is the content of the fourth article.'
+        //     ],
+        //     [
+        //         'id' => 5,
+        //         'title' => 'Fifth Article',
+        //         'content' => 'This is the content of the fifth article.'
+        //     ]
+        // ];
+
+        $articles = Article::all();
 
         return view('articles.index', compact('articles'));
     }
